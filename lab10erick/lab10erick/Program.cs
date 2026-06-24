bbd069962cc497749668f9ee1c8309fcf6e7f986
robbiejude murillo Gonza <erick123465789@gmail.com>
2026-06-23 21:23:47 -0500

    z

--- lab10erick/lab10erick/Program.cs
+++ lab10erick/lab10erick/Program.cs
@@ -81,12 +81,6 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-
-    app.UseSwaggerUI();
-}
 app.UseSwagger();
 app.UseSwaggerUI();
 
